c42f8f3cef5f89c7172df3830f54eea3cd0d07a4
siaslas <huang.treanthony@ufl.edu>
2024-05-24 11:06:37 -0400

    Initial commit with local project files

---
+++ cmake-build-debug/CMakeFiles/SinglyLinkedList.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SinglyLinkedList.dir/main.cpp.o"
+  "CMakeFiles/SinglyLinkedList.dir/main.cpp.o.d"
+  "SinglyLinkedList"
+  "SinglyLinkedList.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SinglyLinkedList.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/SinglyLinkedList.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for SinglyLinkedList.
+# This may be replaced when dependencies are built.
